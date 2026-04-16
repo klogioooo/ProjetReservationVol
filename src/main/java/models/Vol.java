cb751b87cd7a05aa3e9ff6d57b47fabc507a7491
KEVINSINGH261 <kevin.singh261@gmail.com>
2026-04-16 19:07:34 +0200

    correction erreur merge

--- src/main/java/models/Vol.java
+++ src/main/java/models/Vol.java
@@ -1,79 +1,50 @@
 package models;
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+import java.util.List;
+
 public class Vol {
     private String numeroVol;
-    private String depart;
-    private String arrivee;
-    private String dateDepart;
-    private String heureDepart;
-    private int capacite;
+    private Aeroport depart;
+    private Aeroport arrivee;
+    private LocalDateTime dateHeureDepart;
+    private LocalDateTime dateHeureArrivee;
+    private String etat; // Planifié, En cours, Annulé
+    
+    private Avion avionAffecte;
+    private List<Employe> equipage; 
+    private List<Reservation> listeReservations; 
 
-    public Vol(String numeroVol, String depart, String arrivee, String dateDepart, String heureDepart, int capacite) {
+    public Vol(String numeroVol, Aeroport depart, Aeroport arrivee, LocalDateTime dateHeureDepart, LocalDateTime dateHeureArrivee) {
         this.numeroVol = numeroVol;
         this.depart = depart;
         this.arrivee = arrivee;
-        this.dateDepart = dateDepart;
-        this.heureDepart = heureDepart;
-        this.capacite = capacite;
+        this.dateHeureDepart = dateHeureDepart;
+        this.dateHeureArrivee = dateHeureArrivee;
+        this.etat = "Planifié";
+        this.equipage = new ArrayList<>();
+        this.listeReservations = new ArrayList<>();
     }
 
-    public String getNumeroVol() {
-        return numeroVol;
+    public void ajouterMembreEquipage(Employe employe) {
+        this.equipage.add(employe);
     }
 
-    public void setNumeroVol(String numeroVol) {
-        this.numeroVol = numeroVol;
+    public void ajouterReservation(Reservation reservation) {
+        this.listeReservations.add(reservation);
     }
 
-    public String getDepart() {
-        return depart;
-    }
-
-    public void setDepart(String depart) {
-        this.depart = depart;
-    }
-
-    public String getArrivee() {
-        return arrivee;
-    }
-
-    public void setArrivee(String arrivee) {
-        this.arrivee = arrivee;
-    }
-
-    public String getDateDepart() {
-        return dateDepart;
-    }
-
-    public void setDateDepart(String dateDepart) {
-        this.dateDepart = dateDepart;
-    }
-
-    public String getHeureDepart() {
-        return heureDepart;
-    }
-
-    public void setHeureDepart(String heureDepart) {
-        this.heureDepart = heureDepart;
-    }
-
-    public int getCapacite() {
-        return capacite;
-    }
-
-    public void setCapacite(int capacite) {
-        this.capacite = capacite;
-    }
+    public void setAvionAffecte(Avion avion) { this.avionAffecte = avion; }
+    public void setEtat(String etat) { this.etat = etat; }
+    public String getNumeroVol() { return numeroVol; }
 
     @Override
     public String toString() {
-        return "Vol{" +
-                "numeroVol='" + numeroVol + '\'' +
-                ", depart='" + depart + '\'' +
-                ", arrivee='" + arrivee + '\'' +
-                ", dateDepart='" + dateDepart + '\'' +
-                ", heureDepart='" + heureDepart + '\'' +
-                ", capacite=" + capacite +
-                '}';
+        DateTimeFormatter format = DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm");
+        String modeleAvion = (avionAffecte != null) ? avionAffecte.getModele() : "Non assigné";
+        return "Vol " + numeroVol + " | " + depart.getCodeIATA() + " -> " + arrivee.getCodeIATA() + 
+               " | Départ: " + dateHeureDepart.format(format) + " | Avion: " + modeleAvion + " | État: " + etat;
     }
 }
